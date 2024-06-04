ee9d5f67356498eff2480484343ecbb61cd7ae94
Giuliano Furgol <giulianofurgol@hotmail.com>
2024-06-04 18:20:03 +0200

    Project page mobile version have been fixed

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,3 +1,15 @@
+import OneItemBlock from "@/components/oneItemBlock/oneItemBlock";
+import ThreeItemsBlock from "@/components/threeItemsBlock/threeItemsBlock";
+import TwoItemsBlock from "@/components/twoItemsBlock/twoItemsBlock";
+import styles from "./page.module.css";
+
 export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My project: {params.slug}</div>;
+  return (
+    <div className={styles.projectPage}>
+      <div>My project: {params.slug}</div>
+      <ThreeItemsBlock />
+      <TwoItemsBlock />
+      <OneItemBlock />
+    </div>
+  );
 }
