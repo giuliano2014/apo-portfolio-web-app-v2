2b8a978717fb920cf1029cefb2be5594e92fb1a9
Giuliano Furgol <giulianofurgol@hotmail.com>
2024-06-06 16:22:57 +0200

    Drawer have been added

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,5 +1,12 @@
+import { Drawer } from "@/components/ui/drawer/drawer";
+
 const Header = () => {
-  return <h2>Header</h2>;
-}
+  return (
+    <>
+      <h2>Header</h2>
+      <Drawer />
+    </>
+  );
+};
 
 export default Header;
---
+++ app/components/ui/drawer/drawer.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+export function Drawer() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+           Description
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">Whatever here...</div>
+      </SheetContent>
+    </Sheet>
+  );
+}
