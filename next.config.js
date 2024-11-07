b024982713e5dad5c4fa4314e9ad9b3001e43e5b
Giuliano Furgol <giulianofurgol@hotmail.com>
2024-11-07 18:24:26 +0100

    Environment variables have been added

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 module.exports = {
     images: {
-        remotePatterns: [ // @TODO: Remove unused hostnames
+        remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'eu-central-1-shared-euc1-02.graphassets.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placeholderimage.eu',
+                hostname: process.env.NEXT_PUBLIC_IMAGES_HOSTNAME,
             },
         ],
     },
